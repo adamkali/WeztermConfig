75bfef25b4f36d691a3602f7afabb794335220c7
Adam Kalinowski <Adam>
2023-04-10 13:27:49 -0400

    softer

--- wezterm.lua
+++ wezterm.lua
@@ -5,50 +5,49 @@ local function hsl(color, saturation, brightness)
 end
 
 local colors = {
-    Background = hsl(250, 80, 8),
+    Background = hsl(250,50,15),
     Primary = {
-        shade0 = hsl(250, 100, 20),
-        shade1 = hsl(250, 100, 30),
-        shade2 = hsl(250, 100, 50),
-        shade3 = hsl(250, 100, 80),
+        shade0 = hsl(250,100, 25),
+        shade1 = hsl(250,100, 35),
+        shade2 = hsl(250,100, 55),
+        shade3 = hsl(250,100, 85),
     },
     Secondary = {
-        shade0 = hsl(300, 100, 20),
-        shade1 = hsl(300, 100, 40),
-        shade2 = hsl(300, 100, 60),
-        shade3 = hsl(300, 100, 80),
+        shade0 = hsl(300, 75, 20),
+        shade1 = hsl(300, 75, 40),
+        shade2 = hsl(300, 75, 60),
+        shade3 = hsl(300, 75, 80),
     },
     Tertiary = {
-        shade0 = hsl(50, 100, 40),
-        shade1 = hsl(50, 100, 50),
-        shade2 = hsl(50, 100, 70),
-        shade3 = hsl(50, 100, 90),
+        shade0 = hsl(50, 75, 40),
+        shade1 = hsl(50, 75, 50),
+        shade2 = hsl(50, 75, 60),
+        shade3 = hsl(50, 75, 70),
     },
     Quartenary = {
-        shade0 = hsl(275, 90, 20),
-        shade1 = hsl(275, 90, 40),
-        shade2 = hsl(275, 90, 60),
-        shade3 = hsl(275, 90, 80),
+        shade0 = hsl(0, 100, 20),
+        shade1 = hsl(0, 100, 40),
+        shade2 = hsl(0, 100, 60),
+        shade3 = hsl(0, 100, 80),
     },
     Quintary = {
-        shade0 = hsl(180, 100, 25),
-        shade1 = hsl(180, 100, 40),
-        shade2 = hsl(180, 100, 60),
-        shade3 = hsl(180, 100, 90),
+        shade0 = hsl(170, 100, 20),
+        shade1 = hsl(170, 100, 40),
+        shade2 = hsl(170, 100, 60),
+        shade3 = hsl(170, 100, 80),
     }
 }
 
 return {
     default_prog = { 'powershell.exe', '-NoLogo' },
     font = wezterm.font_with_fallback {
-        { family = 'VictorMono Nerd Font', weight = "Bold", stretch = "SemiCondensed" },
-        { family = 'VictorMono Nerd Font', weight = "ExtraBold", stretch = "SemiCondensed" },
+        'Recursive',
+        { family = 'Recursive', weight = "Bold", stretch = "SemiCondensed" },
+        'Symbols Nerd Font Mono',
         'Segoe UI Emoji' 
     },
-    set_environment_variables = {
-      prompt = ' ',
-    },
-    font_size = 14.0,
+    window_background_opacity = 1,
+    font_size = 18.0,
     colors = {
 
         foreground = colors.Secondary.shade3,
